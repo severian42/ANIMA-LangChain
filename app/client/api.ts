6d78480771d42649d1fcd6071d6e4a76c682b147
Beckett <133655553+severian42@users.noreply.github.com>
2023-11-19 20:59:39 +0000

    azuer

--- app/client/api.ts
+++ app/client/api.ts
@@ -1,12 +1,12 @@
 import { getClientConfig } from "../config/client";
-import { ACCESS_CODE_PREFIX, Azure, ServiceProvider } from "../constant";
+import { ACCESS_CODE_PREFIX, ServiceProvider } from "../constant";
 import { ChatMessage, ModelType, useAccessStore } from "../store";
 import { ChatGPTApi } from "./platforms/openai";
 
 export const ROLES = ["system", "user", "assistant"] as const;
 export type MessageRole = (typeof ROLES)[number];
 
-export const Models = ["gpt-3.5-turbo", "gpt-4"] as const;
+export const Models = ["A N I M A"] as const;
 export type ChatModel = ModelType;
 
 export interface RequestMessage {
@@ -116,8 +116,7 @@ export class ClientApi {
       .concat([
         {
           from: "human",
-          value:
-            "Share from [A N I M A]: https://www.animabiomimicry.org",
+          value: "Share from [A N I M A]: https://www.animabiomimicry.org",
         },
       ]);
     // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
